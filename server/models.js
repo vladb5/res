fbd3aadd8970de085bdecdafa912f2083da426b6
169 <vladbr55@ya.ru>
2023-10-13 00:33:29 +0300

    111

--- server/models.js
+++ server/models.js
@@ -2,5 +2,6 @@ const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
-    id: {type: d}
-})
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataT}
+} )
