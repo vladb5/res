184204b9d2475257ebf8c7d608325654ae425fe8
Fill <filimonpas@gmail.com>
2023-10-13 01:42:00 +0300

    jh

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,20 @@
-//1111
+import React, {createContext} from "react";
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import UserStore from './store/UserStore';
+import ArticlrStore from './store/AerticleStore';
+
+export const Context = craateContext(null)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+    <Context.Provider value={{
+        user:new UserStore(),
+        article: new ArticlrStore()
+    }}>
+        <App/>
+    </Context.Provider>
+    </React.StrictMode>
+);
